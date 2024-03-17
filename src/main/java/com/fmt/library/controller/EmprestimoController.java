035b219146b18903270a18064bc841d7c843c71c
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-17 13:18:42 -0300

    feat: create first POST method of EmprestimoController (C in CRUD)

--- src/main/java/com/fmt/library/controller/EmprestimoController.java
+++ src/main/java/com/fmt/library/controller/EmprestimoController.java
@@ -1,4 +1,28 @@
 package com.fmt.library.controller;
 
+import com.fmt.library.model.Emprestimo;
+import com.fmt.library.model.Livro;
+import com.fmt.library.repository.EmprestimoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emprestimo")
 public class EmprestimoController {
+
+    EmprestimoRepository emprestimoRepository;
+
+    public EmprestimoController(EmprestimoRepository emprestimoRepository) {
+        this.emprestimoRepository = emprestimoRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Emprestimo> criarUmEmprestimo(@RequestBody Emprestimo emprestimo) {
+        Emprestimo novoEmprestimo = emprestimoRepository.save(emprestimo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoEmprestimo);
+    }
 }
