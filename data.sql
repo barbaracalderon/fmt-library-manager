227ff9028d296914d3259559e6ccc061fba3074f
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-17 14:43:34 -0300

    script: one line of data to 4 tables

---
+++ data.sql
@@ -0,0 +1,7 @@
+INSERT INTO livro (titulo, autor, anoPublicacao) VALUES ('Cosmos', 'Carl Sagan', 1980);
+
+INSERT INTO membro (nome, endereco, telefone) VALUES ('Bela', 'Rua A, 123', '555-1234');
+
+INSERT INTO bibliotecario (nome, email) VALUES ('Maria', 'maria@exemploemail.com');
+
+INSERT INTO visitante (nome, telefone) VALUES ('Fernanda', '111-1111')
