a023acded79402503f63843f43aab24330f1a0f7
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-17 05:38:28 -0300

    feat: create LivroRepository

---
+++ src/main/java/com/fmt/library/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.fmt.library.repository;
+
+import com.fmt.library.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
