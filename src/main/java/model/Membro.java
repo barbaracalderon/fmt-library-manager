dcbc4b0dfe9cc46f50a09e3192216b769ea340d5
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-16 21:31:37 -0300

    feat: start of Membro model

--- src/main/java/model/Membro.java
+++ src/main/java/model/Membro.java
@@ -1,4 +1,20 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Membro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+    private String endereco;
+    private String telefone;
 }
