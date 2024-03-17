cb0e8d5ef2f4454408fabce1beebbea02251c319
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-17 05:58:54 -0300

    feat: create BibliotecarioRepository

---
+++ src/main/java/com/fmt/library/repository/BibliotecarioRepository.java
@@ -0,0 +1,9 @@
+package com.fmt.library.repository;
+
+import com.fmt.library.model.Bibliotecario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BibliotecarioRepository extends JpaRepository<Bibliotecario, Integer> {
+}
