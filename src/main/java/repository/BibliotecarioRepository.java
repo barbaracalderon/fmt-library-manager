b5eb40ea339db0d57210937066d977707139c784
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-17 05:34:15 -0300

    delete: move packages to correct path

--- src/main/java/repository/BibliotecarioRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Bibliotecario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BibliotecarioRepository extends JpaRepository<Bibliotecario, Integer> {
-}
--- src/main/java/repository/MembroRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Membro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MembroRepository extends JpaRepository<Membro, Integer> {
-}
