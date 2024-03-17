09d6fa5665789ba18a678bcaca485d04180a1fb3
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-17 06:45:41 -0300

    feat: create Membro model

---
+++ src/main/java/com/fmt/library/model/Membro.java
@@ -0,0 +1,62 @@
+package com.fmt.library.model;
+
+import jakarta.persistence.*;
+
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Membro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    public Membro() {
+    }
+
+    public Membro(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
