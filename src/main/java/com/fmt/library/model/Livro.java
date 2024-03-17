79bc42a5a3dbafc30ccdcb7d939814c3692ba032
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-17 05:37:45 -0300

    feat: create model Livro

---
+++ src/main/java/com/fmt/library/model/Livro.java
@@ -0,0 +1,64 @@
+package com.fmt.library.model;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String autor;
+
+    @Column(nullable = false)
+    private Integer anoPublicacao;
+
+    public Livro() {
+    }
+
+    public Livro(String titulo, String autor, Integer anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+}
