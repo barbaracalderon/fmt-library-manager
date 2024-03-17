bfcea269cf99d52f9cd9f68a645229824051e412
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-16 22:14:15 -0300

    feat: start of Membro repository for CRUD

---
+++ src/main/java/repository/MembroRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import model.Membro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MembroRepository extends JpaRepository<Membro, Integer> {
+}
