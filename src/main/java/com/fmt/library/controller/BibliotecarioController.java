aa0976949e39524e298b9214f158d863894804e4
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-17 06:18:37 -0300

    feat: create BibliotecarioController with CRUD operations

---
+++ src/main/java/com/fmt/library/controller/BibliotecarioController.java
@@ -0,0 +1,70 @@
+package com.fmt.library.controller;
+
+import com.fmt.library.model.Bibliotecario;
+import com.fmt.library.model.Livro;
+import com.fmt.library.repository.BibliotecarioRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/bibliotecario")
+public class BibliotecarioController {
+
+    BibliotecarioRepository bibliotecarioRepository;
+
+    public BibliotecarioController(BibliotecarioRepository bibliotecarioRepository) {
+        this.bibliotecarioRepository = bibliotecarioRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Bibliotecario> criarUmBibliotecario(@RequestBody Bibliotecario bibliotecario) {
+        Bibliotecario novoBibliotecario = bibliotecarioRepository.save(bibliotecario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoBibliotecario);
+    }
+
+    @GetMapping
+    public ResponseEntity<ArrayList<Bibliotecario>> buscarTodosOsBibliotecarios() {
+        ArrayList<Bibliotecario> bibliotecarios = (ArrayList<Bibliotecario>) bibliotecarioRepository.findAll();
+        if (bibliotecarios.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(bibliotecarios);
+        }
+    }
+
+    @GetMapping("{idBibliotecario}")
+    public ResponseEntity<Bibliotecario> buscarUmBibliotecario(@PathVariable Integer idBibliotecario) {
+        Optional<Bibliotecario> bibliotecarioEncontrado = bibliotecarioRepository.findById(idBibliotecario);
+        return bibliotecarioEncontrado.map(
+                livro -> new ResponseEntity<>(livro, HttpStatus.FOUND)
+        ).orElseGet(
+                () -> new ResponseEntity<>(HttpStatus.NOT_FOUND)
+        );
+    }
+
+    @PutMapping("{idBibliotecario}")
+    public ResponseEntity<Optional<Bibliotecario>> atualizarUmBibliotecario(@PathVariable Integer idBibliotecario, @RequestBody Bibliotecario bibliotecarioAtualizado) {
+        Optional<Bibliotecario> bibliotecarioParaAtualizar = bibliotecarioRepository.findById(idBibliotecario);
+        if (bibliotecarioParaAtualizar.isPresent()) {
+            Bibliotecario bibliotecarioExistente = bibliotecarioParaAtualizar.get();
+            bibliotecarioExistente.setNome(bibliotecarioAtualizado.getNome());
+            bibliotecarioExistente.setEmail(bibliotecarioAtualizado.getEmail());
+            bibliotecarioExistente.setSenha(bibliotecarioAtualizado.getSenha());
+            Bibliotecario bibliotecarioAtualizadoSalvo = bibliotecarioRepository.save(bibliotecarioExistente);
+            return ResponseEntity.ok(Optional.of(bibliotecarioAtualizadoSalvo));
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("{idBibliotecario}")
+    public ResponseEntity<Void> deletarUmBibliotecario(@PathVariable Integer idBibliotecario) {
+        bibliotecarioRepository.deleteById(idBibliotecario);
+        return ResponseEntity.noContent().build();
+    }
+
+}
